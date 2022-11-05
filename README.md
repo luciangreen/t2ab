c7c229f78087f5c8634c459337e1388c80806e64
Lucian Green <luciangreen@lucianacademy.com>
2022-11-06 01:21:45 +1100

    First Upload

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# t2ab
-Text to Algorithm Breasoning
+# t2ab - text to algorithm breasoning
+
+Text to Algorithm Breasoning is a SWI-Prolog algorithm that prepares to breason out file.txt (in the Text to Breasonings folder) with an algorithm for each word's object.  Breasoning is thinking of the x, y and z dimensions of objects, necessary for meeting grades.
+
+# Getting Started
+
+Please read the following instructions on how to install the project on your computer for breasoning out algorithms.
+
+# Prerequisites
+
+* Please download and install SWI-Prolog for your machine at `https://www.swi-prolog.org/build/`.
+
+# 1. Install manually
+
+Download <a href="http://github.com/luciangreen/t2ab/">this repository</a>, the <a href="https://github.com/luciangreen/listprologinterpreter">List Prolog Interpreter Repository</a> and the <a href="https://github.com/luciangreen/Text-to-Breasonings">Text to Breasonings Repository</a>.
+
+# 2. Or Install from List Prolog Package Manager (LPPM)
+
+* Download the <a href="https://github.com/luciangreen/List-Prolog-Package-Manager">LPPM Repository</a>:
+
+```
+git clone https://github.com/luciangreen/List-Prolog-Package-Manager.git
+cd List-Prolog-Package-Manager
+swipl
+['lppm'].
+lppm_install("luciangreen","t2ab").
+halt
+```
+
+# Running
+
+* In Shell:
+`cd t2ab`
+`swipl`
+`['t2ab.pl'].`    
+
+* Before running texttobr, think of two radio buttons put on recordings, put through with prayer, nut and bolt, quantum box prayer 1, 1, 0.5 cm and 1, 1, 0.5 cm.
+
+* Follow instructions in https://github.com/luciangreen/Text-to-Breasonings/blob/master/Instructions_for_Using_texttobr(2).pl.txt when using texttoalg, texttobr, or texttobr2 to avoid medical problems.
+
+* Enter the following to breason out object algorithms:
+`t2ab(u,u,u,u).`    
+
+
+# Authors
+
+Lucian Green - Initial programmer - <a href="https://www.lucianacademy.com/">Lucian Academy</a>
+
+# License
+
+I licensed this project under the BSD3 License - see the LICENSE.md file for details
---
+++ t2ab.pl
@@ -0,0 +1,388 @@
+% ['../Text-to-Breasonings/text_to_breasonings.pl'].
+% W is 50*4,texttobr2(u,u,u,u,false,false,false,false,false,false,W)
+% where W is the number of words to read
+% and there are approximately 4 words per algorithm.
+
+%% Important: See instructions for using texttobr.pl at https://lucianpedia.wikia.com/wiki/Instructions_for_Using_texttobr(2).pl .
+
+%% use_module(library(pio)). %% In la_strings
+%% use_module(library(dcg/basics)). %% In la_strings
+
+%% texttobr2 - converts file stream to dimensions of objects represented by the words
+%% has object name as separate field for new users of texttobr to verify breasonings by hand
+%% brdict1.txt contains word and object name, brdict2.txt contains object name and x, y and z
+
+%% texttobr2(Runs,File,StringtoBreason,BreasoningLimit).
+%:- include('../listprologinterpreter/la_strings.pl').
+:- include('../Text-to-Breasonings/mergetexttobrdict.pl').
+
+:-include('../Text-to-Breasonings/texttobr.pl').
+
+:- include('../listprologinterpreter/listprolog').
+:- include('../Text-to-Breasonings/texttobr2qb').
+
+%:- include('../listprologinterpreter/la_strings').
+
+%% Brth is true or false
+t2ab(N1,Filex1,Stringx1,M1) :-
+	t2ab(N1,Filex1,Stringx1,M1,0).
+t2ab(N1,Filex1,Stringx1,M1,Words_to_read) :-
+	retractall(complete_display(_)),
+	assertz(complete_display(false)),
+
+	retractall(words_to_read(_)),
+	assertz(words_to_read(Words_to_read)),
+	
+	((number(N1),N=N1)->true;
+	(N1=u,N=1)),
+
+	((Filex1=u,Filex="../Text-to-Breasonings/file.txt")->true;
+	Filex=Filex1),
+
+	((number(M1),M=M1)->true;
+	M=all), %% If m1 is undefined or all then m=all
+
+	prep(List1,BrDict03,AlgDict_x,AlgDict,Filex,Stringx1,M),
+	br2(List1,BrDict03,BrDict2,AlgDict_x,AlgDict_x2,AlgDict,AlgDict2,N),
+	sort(BrDict2,BrDict3),
+	(BrDict03=BrDict3->true;
+	(open_s("../Text-to-Breasonings/brdict1.txt",write,Stream),
+%%	string_codes(BrDict3),
+	write(Stream,BrDict3),
+	close(Stream))),
+
+	sort(AlgDict_x2,AlgDict_x3),
+	(AlgDict_x=AlgDict_x3->true;
+	(open_s("algdict1.txt",write,Stream2),
+%%	string_codes(BrDict3),
+	term_to_atom(AlgDict_x3,AlgDict_x31),
+	write(Stream2,AlgDict_x31),
+ 	close(Stream2))),
+ 	
+	sort(AlgDict2,AlgDict3),
+	(AlgDict=AlgDict3->true;
+	(open_s("algdict2.txt",write,Stream3),
+	term_to_atom(AlgDict3,AlgDict31),
+%%	string_codes(BrDict3),
+	write(Stream3,AlgDict31),
+ 	close(Stream3))),
+
+ 	length(List1,List1_length_a),
+ 	Dividend_a is ceiling(List1_length_a/250),
+ 	Dividend_b is Dividend_a*3, % for graciously giving
+ 	texttobr2_a(Dividend_b,meditation),
+ 	texttobr2_a(Dividend_b,medicine),
+ 	texttobr2_a(Dividend_b,pedagogy),
+ 	
+ 	!.
+
+%% Truncates the list if m is not undefined and m is greater than or equal to the length of string0
+truncate(List1,M,String0) :-
+	((number(M),length(String0,M),
+	append(String0,_,List1))->true;
+	String0=List1),!.
+	
+prep(List,BrDict03,AlgDict_x,AlgDict,Filex,Stringx1,M) :-
+	phrase_from_file_s(string(BrDict0), "../Text-to-Breasonings/brdict1.txt"),
+	%%Chars="’",
+	SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!'0123456789",
+	%%split_string(BrDict0,SepandPad,SepandPad,BrDict01),
+%%writeln([brDict0,BrDict0]),
+%%writeln([brdict1]),
+	splitfurther(BrDict0,BrDict01),
+%%writeln([brDict01,BrDict01]),
+	%%char_code(Escape,27),
+	%%delete(BrDict01,[Escape,_,_,_,_],BrDict021),
+%%writeln([brDict021,BrDict021]),
+	%%char_code(Apostrophe,8217),
+	%%delete(BrDict021,[Apostrophe,_,_,_,_],BrDict02),
+%%writeln([brDict02,BrDict02]),
+	sort(BrDict01,BrDict03),
+%%writeln([brDict03,BrDict03]),
+	length(BrDict03,Length0),write("Number of words in dictionary: "), writeln(Length0),
+	
+	%%writeln(''),
+	%%writeln([brdict2]),
+	phrase_from_file_s(string(BrDict0t), "algdict1.txt"),
+	%%Chars="’",
+	%%split_string(BrDict0,SepandPad,SepandPad,BrDict01),
+%%writeln([brDict0,BrDict0]),
+%	splitfurthert(BrDict0t,BrDict01t),
+string_atom(BrDict0t,Atom),atom_to_term(Atom,BrDict01t,_),
+
+%%writeln([brDict01,BrDict01]),
+	%%delete(BrDict01t,[Escape,_,_,_,_],BrDict021t),
+%%writeln([brDict021,BrDict021]),
+	%%delete(BrDict021t,[Apostrophe,_,_,_,_],BrDict02t),
+%%writeln([brDict02,BrDict02]),
+	sort(BrDict01t,AlgDict_x),
+	
+% br_freq		%B=AlgDict_x,A=BrDict03,findall([DL,C,"\n"],(member([C,_,_,_],B),findall(_,member([_,C],A),D),length(D,DL)),E),sort(E,F),reverse(F,G),writeln([br_freq,G]),
+
+%%writeln([brDict03,BrDict03]),
+	length(AlgDict_x,Length0t),write("Number of unique algorithm names in dictionary: "), writeln(Length0t),
+	
+	%trace,
+	
+	phrase_from_file_s(string(AlgDict0), "algdict2.txt"),
+	%%Chars="’",
+	%%split_string(BrDict0,SepandPad,SepandPad,BrDict01),
+%%writeln([brDict0,BrDict0]),
+%%writeln([brdict1]),
+string_atom(AlgDict0,Atom1),atom_to_term(Atom1,AlgDict01,_),
+	%%splitfurther(BrDict0,BrDict01),
+%%writeln([brDict01,BrDict01]),
+	%%char_code(Escape,27),
+	%%delete(BrDict01,[Escape,_,_,_,_],BrDict021),
+%%writeln([brDict021,BrDict021]),
+	%%char_code(Apostrophe,8217),
+	%%delete(BrDict021,[Apostrophe,_,_,_,_],BrDict02),
+%%writeln([brDict02,BrDict02]),
+	sort(AlgDict01,AlgDict),
+%%writeln([brDict03,BrDict03]),
+	length(AlgDict,Length01),write("Number of algorithms in dictionary: "), writeln(Length01),
+	
+	
+		
+	
+	((Stringx1=u,
+	phrase_from_file_s(string(String00), Filex))->true;
+	String00=Stringx1),
+	
+	split_string(String00,SepandPad,SepandPad,List1),
+	%%split_string_onnonletter(String00,List1),
+
+	truncate(List1,M,List),
+
+	/**replace0(String0,[8221, 8220], 34, SepandPad, M, String1),
+	replace0(String1,[8216, 8217], 39, SepandPad, M, String2),
+	replace0(String2,[8259, 8211, 8212], 45, SepandPad, M, String3),
+	replace0(String3,[160], 32, SepandPad, M, List),
+	**/
+
+%%atom_codes(Atom999,String),writeln([atom999,Atom999]),
+
+%%writeln([list,List]),
+	%%delete(List,Escape,List11),
+%%writeln([list11,List11]),
+	%%delete(List11,Apostrophe,List1),
+%%writeln([list1,List1]),
+	length(List,Length1),write("Number of words to breason out in file: "), writeln(Length1),
+	sort(List,List2),
+%%writeln([list2,List2]),
+	length(List2,Length2),write("Number of unique words in file: "), writeln(Length2),
+	
+
+(complete_display(true)->
+	((Stringx1=u, %% Use file, not string as input.
+	
+	%%maplist(downcase_atom, List2, List3),
+	maplist(string_lower, List2, List3),
+	
+%%writeln([list3,List3]),
+	towords3(BrDict03,[],BrDict04,[],_ObjectNames,[],AllUsedNames),
+	
+	findall(BrDict04tt,(member([BrDict04tt,_],AlgDict_x)),BrDict04t),
+	findall(BrDict04tt0,(member([_,BrDict04tt0],AlgDict_x)),BrDict04t0),
+	findall(BrDict04tt1,(member([BrDict04tt1,_],AlgDict)),BrDict04t1),
+	%towords2(AlgDict_x,[],BrDict04t),
+
+%%writeln([brDict04,BrDict04]),
+	subtract(List3,BrDict04,D1),
+%%writeln([list3,brDict04,d1,List3,BrDict04,D1]),
+%%writeln(["subtract(BrDict04,List3,D1).",List3,BrDict04,D1]),	
+	length(D1,Length01),Difference is abs(Length01),write("Number of words remaining to define: "), writeln(Difference),
+
+	subtract(AllUsedNames,BrDict04t,D2),
+	%%delete(D21,'',D2),
+	length(D2,Length01t),Differencet is abs(Length01t),write("Number of undefined algorithm names: "), writeln(Differencet),
+	%% writeln([undefinedbreasonings,D2]), %% Print undefined breasonings
+
+	%%delete(D31,'',D3),
+	subtract(BrDict04t,AllUsedNames,D3),
+	length(D3,Length01t2),Differencet2 is abs(Length01t2),write("Number of orphaned algorithm names: "), writeln(Differencet2),
+	
+	
+	
+	/*
+		subtract(List3,BrDict04,D1),
+%%writeln([list3,brDict04,d1,List3,BrDict04,D1]),
+%%writeln(["subtract(BrDict04,List3,D1).",List3,BrDict04,D1]),	
+	length(D1,Length01),Difference is abs(Length01),write("Number of algorithms remaining to define: "), writeln(Difference),
+*/
+
+	subtract(BrDict04t0,BrDict04t1,D21),
+	%%delete(D21,'',D2),
+	length(D21,Length01t1),Differencet1 is abs(Length01t1),write("Number of undefined algorithms: "), writeln(Differencet1),
+	%% writeln([undefinedbreasonings,D2]), %% Print undefined breasonings
+
+	%%delete(D31,'',D3),
+	subtract(BrDict04t1,BrDict04t0,D31),
+	length(D31,Length01t21),Differencet21 is abs(Length01t21),write("Number of orphaned algorithms: "), writeln(Differencet21)
+
+		%%,writeln([orphanedbreasonings,D3]) %% Print orphaned breasonings
+	
+			
+	
+	
+
+)->true;(string(Filex),writeln("Number of words, unique words, words remaining to define, undefined algorithm names, orphaned algorithm names, undefined algorithms and orphaned algorithms skipped for speed when breasoning out a string.")));true)
+
+,!.
+
+br2(_,A,A,B,B,C,C,0) :- !.
+br2(List1,BrDict03,BrDict2,AlgDict_x,AlgDict_x2,AlgDict,AlgDict2,N1) :-
+	br(List1,BrDict03,BrDict21,AlgDict_x,AlgDict_x21,AlgDict,AlgDict21),
+	N2 is N1-1,
+	br2(List1,BrDict21,BrDict2,AlgDict_x21,AlgDict_x2,AlgDict21,AlgDict2,N2),!.
+
+towords2([],A,A) :- !.
+towords2(BrDict03,A,B) :-
+	BrDict03=[[Word,_,_,_]|Rest],
+	%%atom_string(Atom,Word),
+	append(A,[Word],C),
+	towords2(Rest,C,B).
+
+towords2a([],A,A) :- !.
+towords2a(BrDict03,A,B) :-
+	BrDict03=[[Word,_]|Rest],
+	%%atom_string(Atom,Word),
+	append(A,[Word],C),
+	towords2a(Rest,C,B).
+
+towords3([],A,A,C,C,D,D) :- !.
+towords3(BrDict03,A,B,D,E,G,H) :-
+	BrDict03=[[Word1,Word2]|Rest],
+	(Word2=""->append(G,[Word1],I)->true;
+	append(G,[Word2],I)),
+	append(A,[Word1],C),
+	append(D,[Word2],F),
+	towords3(Rest,C,B,F,E,I,H).
+
+string(String) --> list(String).
+
+list([]) --> [].
+list([L|Ls]) --> [L], list(Ls).
+
+splitfurther(BrDict01,N) :-
+	   phrase(file0(N),BrDict01).
+	
+file0(N) --> "[", file(N), "]", !.
+file0([]) --> [].
+
+%%file([]) --> [].
+file([L|Ls]) --> entry(L),",",
+%%{writeln(L)}, %%***
+file(Ls), !. %% file(Ls),{M=[Ls]})), !. %%, {writeln(["l",L])},",", file(Ls), {writeln(["ls",Ls])},!. %%, {append(L,Ls,M)}, !.	
+file([L]) --> entry(L), 
+%%{writeln(L)},
+!. %%(entry(L),{M=L});{M=[],(writeln("Warning - Entry in incorrect format.")
+%%,abort
+%%)}, !.
+
+entry([Word2,Word4]) -->
+		"[", word(Word), {string_codes(Word2,Word),string(Word2)},
+		",",
+           word(Word3), {string_codes(Word4,Word3),string(Word4)},
+           "]".
+
+splitfurthert(BrDict01,N) :-
+	   phrase(file0t(N),BrDict01).
+	
+file0t(N) --> "[", filet(N), "]", !.
+file0t([]) --> [].
+
+%%file([]) --> [].
+filet([L|Ls]) --> entryt(L),",",
+%%{writeln(L)}, %%***
+filet(Ls), !. %% file(Ls),{M=[Ls]})), !. %%, {writeln(["l",L])},",", file(Ls), {writeln(["ls",Ls])},!. %%, {append(L,Ls,M)}, !.	
+filet([L]) --> entryt(L), 
+%%{writeln(L)},
+!. %%(entry(L),{M=L});{M=[],(writeln("Warning - Entry in incorrect format.")
+%%,abort
+%%)}, !.
+
+entryt([Word2,X3,Y3,Z3]) -->
+		"[", word(Word), {string_codes(Word2,Word),string(Word2)},
+		",",
+	      digits(X),",",{atom_codes(X2,X),atom_number(X2,X3),number(X3)},
+           digits(Y),",",{atom_codes(Y2,Y),atom_number(Y2,Y3),number(Y3)},
+           digits(Z),{atom_codes(Z2,Z),atom_number(Z2,Z3),number(Z3)},
+           "]".
+
+word([X|Xs]) --> [X], {char_type(X,csymf)->true;(X=27->true;X=8217)}, word(Xs), !.
+%%word([X]) --> [X], {char_type(X,csymf);(X=27;X=8217)}, !.
+word([]) --> [].
+
+digits([X|Xs]) --> [X], {(char_type(X,digit)->true;(string_codes(Word2,[X]),Word2="."))}, digits(Xs), !.
+%%digits([X]) --> [X], {(char_type(X,digit);(string_codes(Word2,[X]),Word2="."))}, !.
+digits([]) --> [].
+
+br([],B,B,C,C,D,D) :-
+	!.
+br([Word|Words],BrDict,BrDict2,AlgDict4,AlgDict5,AlgDict6,AlgDict7) :-
+	downcase_atom(Word, Word2), atom_string(Word2,Word3),
+	
+	words_to_read(WR1),
+	(WR1>0->(writeln(WR1),write(Word),
+	texttobr2(3),nl,sleep(0.12),
+	WR2 is WR1-1,
+	retractall(words_to_read(_)),
+	assertz(words_to_read(WR2)));
+	true),
+	
+	/**member([Word3,X,Y,Z],AlgDict4) -> %% This feature is a bug because words in brdict2 shouldn't necessarily be the words in brdict1
+	%%(append(BrDict,[[Word3,""]],BrDict3), BrDict3t=AlgDict4,
+	%%br(Words,BrDict3,BrDict2,BrDict3t,AlgDict5))
+	%%;
+	%%(**/
+	
+	%%(member([Word3,X,Y,Z],AlgDict4) -> %% This feature is a bug because words in brdict1 should correspond to those in brdict2
+	%%(atom_concat("The breasoning for ", Word3, P1),
+	%%atom_concat(P1, " is defined.  Enter object name (without spaces), if different for ", Prompt));
+	%Prompt="Enter object name (without spaces), if different for "),
+	
+	%%writeln([word3,Word3]),
+	
+	(member([Word3,String4],BrDict)-> 
+	BrDict3=BrDict;
+	((repeat,
+	write("Enter object name (without spaces), if different for "), writeln(Word3),read_string(user_input, "\n", "\r", _End2, String2),split_string(String2, "", " ", String3),String3=[String4]),
+	%%*brth(Word3,_Brth),
+
+(String4=""->String5=Word3;String5=String4),
+	append(BrDict,[[Word3,String5]],BrDict3),
+	texttobr2(1,u,String5,1))),
+
+	downcase_atom(String5, String52), atom_string(String52,String53),
+
+	(member([String53,_X],AlgDict4)->
+	AlgDict41=AlgDict4;
+	
+	((repeat,
+	write("Enter algorithm name for "), writeln(String53),read_string(user_input, "\n", "\r", _, String21),split_string(String21, "", " ", String31),String31=[String41]),
+	%%*brth(Word3,_Brth),
+
+(String41=""->String51=String53;String51=String41),
+	append(AlgDict4,[[String53,String51]],AlgDict41),
+	texttobr2(1,u,String51,1))),
+
+	downcase_atom(String51, String521), atom_string(String521,String531),
+
+	(member([String531,_Y],AlgDict6)->
+	AlgDict61=AlgDict6;
+	
+	((repeat,
+	write("Enter Prolog algorithm for "), writeln(String531),read_string(user_input, "\n", "\r", _End, String),%split_string(String, ",", " ", Values),Values=[X1,Y1,Z1],number_string(X,X1),number_string(Y,Y1),number_string(Z,Z1)),
+	append(AlgDict6,[[String531,String]],AlgDict61)))),
+	%%*brth(String53,_Brth2),
+	%%write("br(\'"),write(Word3),writeln("\',)."),
+%%	writeln([Word3,X,Y,Z]),
+	%%write(' '),
+	
+	
+
+br(Words,BrDict3,BrDict2,AlgDict41,AlgDict5,AlgDict61,AlgDict7).
+	%%).
+
+%% finds unknown words, asks for their br in form "n of m: word", verify, (can go back x) append and sort, save
